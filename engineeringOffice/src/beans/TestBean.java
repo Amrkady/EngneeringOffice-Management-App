9932ed144e67af122d9037753358e1598e094992
haitham said <haitham.soliman94@gmail.com>
2020-03-12 10:40:11 -0700

    engOffice Project

---
+++ engineeringOffice/src/beans/TestBean.java
@@ -0,0 +1,25 @@
+package beans;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class TestBean {
+
+	private String name="haitham";
+
+	@PostConstruct
+	public void init() {
+		System.out.println("init has working");
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
