e4c2c504c57ea0b4cfe362340ea0aa70d5380c6b
amr <amralkady89@gmail.com>
2020-03-14 18:16:23 +0200

    entities

---
+++ engineeringOffice/src/com/entities/Departments.java
@@ -0,0 +1,25 @@
+package com.entities;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="users")
+public class Departments {
+	
+	@Id
+	@GenericGenerator(name = "generator", strategy = "increment")
+	@GeneratedValue(generator = "generator")
+	@Column(name = "id")
+	private Integer userId;
+	@Column(name = "LOGIN_NAME", nullable = false)
+	private String loginName;
+	@Column(name = "PASSWORD", nullable = false)
+	private String password;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "role_id")
+	private Integer roleId;
+	
+}
