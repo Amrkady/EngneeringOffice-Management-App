654981201578aede4d3c1ef9a31e72793cc154e0
amr <amralkady89@gmail.com>
2020-03-15 20:43:04 +0200

    sand page

---
+++ engineeringOffice/src/com/services/SandService.java
@@ -0,0 +1,14 @@
+package com.services;
+
+import java.util.List;
+
+import com.entities.Bills;
+import com.entities.Users;
+
+public interface SandService {
+	
+   boolean addSand(Bills bill);
+   
+	
+
+}
